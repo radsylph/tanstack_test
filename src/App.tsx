0fbebb64272d4de091a4ac14e80718506e663069
andres alvarez <andresalvarezsilva6@gmail.com>
2025-06-22 13:43:50 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import './App.css'
+
+function App() {
+
+  const query = useQuery({
+  })
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
